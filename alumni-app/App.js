f24b055d6e965b975e882cf92fbba45457be92c9
SANKARMAHARAJAN <sankarmaharajan03@gmail.com>
2023-10-13 21:17:09 +0530

    merge before commits

--- alumni-app/App.js
+++ alumni-app/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.loginContainer}>
+        <Text style={styles.loginHeader}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+        />
+        <Button
+          title="Login"
+          onPress={() => {
+            // Handle login logic here
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +31,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#faf0e6',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  loginContainer: {
+    width: '80%',
+    height: '50%',
+    backgroundColor: '#c0c0c0', // Background color for the login form
+    borderRadius: 10, // Small edge curved like a square
+    padding: 20,
+  },
+  loginHeader: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5, // To round the input edges
+    marginBottom: 10,
+    paddingLeft: 10,
   },
 });
