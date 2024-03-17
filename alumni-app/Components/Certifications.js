f55c488aeaa90ae0e34e76c72455c7b6974b2895
Sabarigirivasan10 <sabarigirivasan05@gmail.com>
2024-03-17 22:27:07 +0530

    Commit

--- alumni-app/Components/Certifications.js
+++
@@ -1,9 +0,0 @@
-import react from "react"
-import { Text,View,TouchableOpacity } from "react-native"
-
-const Certifications=()=>{
-    return(
-        <Text> Certificates</Text>
-    )
-}
-export default Certifications
--- alumni-app/api_manager/Api.js
+++
@@ -1,2 +0,0 @@
-export const Login_URL='https://0e71-121-200-52-130.ngrok-free.app/alumni/login/'
-
